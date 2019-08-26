f3fe9404faed51dc718d598021552a5e3c292dd4
zdongxiao <zdongxiao@foxmail.com>
2019-08-26 16:57:44 +0800

    ss

---
+++ src/main/java/com/example/pojo/User.java
@@ -0,0 +1,49 @@
+package com.example.pojo;
+
+/**
+ * com.example.pojo
+ *
+ * @author zhangdongxiao
+ * @create 2019-08-26 16:56
+ * @desc
+ */
+
+public class User {
+
+    private String id;
+    private String userName;
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
