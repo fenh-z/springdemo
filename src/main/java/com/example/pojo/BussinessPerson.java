daa3a3ee158c57493e91f000ddbca9c665eb71c6
zdongxiao <zdongxiao@foxmail.com>
2019-08-27 09:17:42 +0800

    ss

--- src/main/java/com/example/pojo/BussinessPerson.java
+++ src/main/java/com/example/pojo/BussinessPerson.java
@@ -23,31 +23,31 @@ public class BussinessPerson implements BeanNameAware, BeanFactoryAware, Applica
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println(this.getClass().getSimpleName() + "调用 BeanFactoryAware 的 setBeanFactory ");
+//        System.out.println(this.getClass().getSimpleName() + "调用 BeanFactoryAware 的 setBeanFactory ");
     }
 
     @Override
     public void setBeanName(String s) {
-        System.out.println(this.getClass().getSimpleName() + "调用 BeanNameAware 的 setBeanName");
+//        System.out.println(this.getClass().getSimpleName() + "调用 BeanNameAware 的 setBeanName");
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.println(this.getClass().getSimpleName() + " DisposableBean 方法");
+//        System.out.println(this.getClass().getSimpleName() + " DisposableBean 方法");
     }
 
     @PreDestroy
     public void destroy0() throws Exception {
-        System.out.println(this.getClass().getSimpleName() + " 注解 PreDestroy 定义的自定义销毁方法");
+//        System.out.println(this.getClass().getSimpleName() + " 注解 PreDestroy 定义的自定义销毁方法");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println(this.getClass().getSimpleName() + "调用 InitializingBean的 afterPropertiesSet 方法");
+//        System.out.println(this.getClass().getSimpleName() + "调用 InitializingBean的 afterPropertiesSet 方法");
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println(this.getClass().getSimpleName() + "调用 ApplicationContextAware 的 setApplicationContext");
+//        System.out.println(this.getClass().getSimpleName() + "调用 ApplicationContextAware 的 setApplicationContext");
     }
 }
