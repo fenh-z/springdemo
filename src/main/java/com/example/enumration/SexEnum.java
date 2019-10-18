20989f5bd18c9d0cf19fa266f1b5ffeefa4ca8fe
zdongxiao <zdongxiao@foxmail.com>
2019-10-18 14:25:36 +0800

    新增dbcp2数据库链接管理

--- src/main/java/com/example/enumration/SexEnum.java
+++ src/main/java/com/example/enumration/SexEnum.java
@@ -8,6 +8,21 @@ public enum SexEnum {
     private int id;
     private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     SexEnum(int id, String name) {
         this.id = id;
